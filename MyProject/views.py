a9d0d79556c48fa27c848fce15237500f74a19d3
Seelam Sanjay Ganesh <51526260+Sanju525@users.noreply.github.com>
2021-01-23 16:43:08 +0530

    Update views.py

--- MyProject/views.py
+++ MyProject/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 from . import settings
-
+import os
 
 # Create your views here.
 
 def index(request):
+    print(os.environ['DEBUG_VALUE'])
     return render(request, 'index.html')
