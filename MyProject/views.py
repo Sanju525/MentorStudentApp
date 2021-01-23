3b8989d82a99f7377459498d12fbd8d7e698555f
Seelam Sanjay Ganesh <51526260+Sanju525@users.noreply.github.com>
2021-01-23 17:12:27 +0530

    Update views.py

--- MyProject/views.py
+++ MyProject/views.py
@@ -6,6 +6,6 @@ import os
 # Create your views here.
 
 def index(request):
-    print(os.environ['DEBUG_VALUE'])
+    print(os.environ['DEBUG_KEY'])
     print("Secret Key",os.environ.get('SECRET_KEY'))
     return render(request, 'index.html')
